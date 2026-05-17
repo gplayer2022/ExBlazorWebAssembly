a347ec175de381411713b6b40234ffacdced853a
gplayer2022 <gplayers@ymail.ne.jp>
2026-05-18 00:51:24 +0900

    deploy

--- publish.cmd
+++ publish.cmd
@@ -5,8 +5,8 @@ cd bin/Release/net8.0/publish/wwwroot
 
 # gh-pagesブランチへデプロイ
 git init
-git remote add origin https://github.com/gplayer2022/ExBlazorWebAssembly.git
+git remote add github_gplayer2022 git@github_gplayer2022:gplayer2022/ExBlazorWebAssembly.git
 git checkout -b gh-pages
 git add .
 git commit -m "deploy"
-git push -f origin gh-pages
+git push -f github_gplayer2022 gh-pages
